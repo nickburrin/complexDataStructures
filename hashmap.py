6ae678956ecf816f57cff85353d11435bdaafe30
NickBurrin <nick.burrin@faithlife.com>
2019-07-21 17:59:42 -0700

    Store and retrieve key,value pairs in the HashMap

--- hashmap.py
+++ hashmap.py
@@ -3,3 +3,19 @@ class HashMap:
     def __init__(self, array_size):
         self.array_size = array_size
         self.array = [None for item in range(array_size)]
+
+    def hash(self, key):
+        key_bytes = key.encode()
+        hash_code = sum(key_bytes)
+        return hash_code
+    
+    def compressor(self, hash_code):
+        return hash_code % self.array_size
+
+    def assign(self, key, value):
+        index = self.compressor(self.hash(key))
+        self.array[index] = value
+
+    def retrieve(self, key):
+        index = self.compressor(self.hash(key))
+        return self.array[index]
---
+++ script.py
@@ -0,0 +1,6 @@
+from hashmap import HashMap
+
+hashMap = HashMap(20)
+hashMap.assign('gneiss', 'metamorphic')
+
+print(hashMap.retrieve('gneiss'))
