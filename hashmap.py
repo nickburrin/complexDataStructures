e26a576a191ef51e32e6ae833c1d9702cbc3cba5
NickBurrin <nick.burrin@faithlife.com>
2019-07-21 18:44:52 -0700

    Implementing open addressing solution for hash collisions

--- hashmap.py
+++ hashmap.py
@@ -1,21 +1,68 @@
 
+## TODO: How would you delete a key-value pair from this hash map?
+## TODO: Parts of the code are a little repetitive, how would you factor these roles differently?
+## TODO: What should your hash map do if a key-value is added and the array is full? How does this hash map handle that?
+
 class HashMap:
     def __init__(self, array_size):
         self.array_size = array_size
         self.array = [None for item in range(array_size)]
 
-    def hash(self, key):
+    def hash(self, key, count_collisions = 0):
         key_bytes = key.encode()
         hash_code = sum(key_bytes)
-        return hash_code
+        return hash_code + count_collisions
     
     def compressor(self, hash_code):
         return hash_code % self.array_size
 
     def assign(self, key, value):
         index = self.compressor(self.hash(key))
-        self.array[index] = value
+        currentValue = self.array[index]
+        if currentValue is None:
+            self.array[index] = [key, value]
+        else:
+            if currentValue[0] == key:
+                self.array[index] = [key, value]
+            else:
+                # Collision occurred
+                number_collisions = 1
+                while(currentValue[0] != key):
+                    new_hash_code = self.hash(key, number_collisions)
+                    new_array_index = self.compressor(new_hash_code)
+                    currentValue = self.array[new_array_index]
+
+                    if currentValue is None:
+                        self.array[new_array_index] = [key, value]
+                        return
+
+                    if currentValue[0] == key:
+                        self.array[new_array_index] = [key, value]
+                        return
+
+                    number_collisions += 1 
+
 
     def retrieve(self, key):
         index = self.compressor(self.hash(key))
-        return self.array[index]
+        possible_item = self.array[index]
+        if possible_item is None:
+            return None
+        else:
+            if possible_item[0] == key:
+                return possible_item[1]
+            else:
+                retrieval_collisions = 1
+                while possible_item[0] != key:
+                    new_hash_code = self.hash(key, retrieval_collisions)
+                    retrieving_array_index = self.compressor(new_hash_code)
+                    possible_item = self.array[retrieving_array_index]
+
+                    if possible_item is None:
+                        return None
+
+                    if possible_item[0] == key:
+                        return possible_item[1]
+
+                    retrieval_collisions += 1 
+
--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 from hashmap import HashMap
 
 hashMap = HashMap(20)
+hashMap.assign('gabbro', 'igneous')
+hashMap.assign('sandstone', 'sedimentary')
 hashMap.assign('gneiss', 'metamorphic')
 
+print(hashMap.retrieve('gabbro'))
+print(hashMap.retrieve('sandstone'))
 print(hashMap.retrieve('gneiss'))
