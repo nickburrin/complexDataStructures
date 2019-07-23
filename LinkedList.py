62b7a395e9813c954ec74f3769c87de455f09938
Nick Burrin <nickburrin18@gmail.com>
2019-07-22 21:54:51 -0700

    Reworking the LinkedList and Node class

--- LinkedList.py
+++ LinkedList.py
@@ -1,57 +1,23 @@
 from Node import Node;
 
 class LinkedList:
-    def __init__(self, value = None):
-        self.head_node = Node(value);
+    def __init__(self, head_node=None):
+        self.head_node = head_node
+    
+    def insert(self, new_node):
+        current_node = self.head_node
 
-    def get_head_node(self):
-        return self.head_node;
+        if not current_node:
+            self.head_node = new_node
 
-    def insert_beginning(self, value):
-        new_node = Node(value);
-        new_node.set_next_node(self.head_node);
-        self.head_node = new_node;
+        while(current_node):
+            next_node = current_node.get_next_node()
+            if not next_node:
+                current_node.set_next_node(new_node)
+            current_node = next_node
 
-    def insert_end(self, value):
-        tail = self.get_tail_node();
-        tail.set_next_node(Node(value));
-
-    def get_tail_node(self):
-        curr = self.head_node;
-        while (curr.get_next_node()):
-            curr = curr.get_next_node()
-        return curr;
-
-    def get_node_with_value(self, value):
-        curr = self.head_node;
-        while curr.get_value() != value:
-            if curr.get_next_node() == None:
-                print('** Could not find node with value: {} **'.format(value));
-                return None;
-            curr = curr.get_next_node();
-        return curr;
-
-    def stringify_list(self):
-        current = self.head_node;
-        output = "";
-        while current.get_next_node() != None:
-            output += str(current.get_value()) + "\n";
-            current = current.get_next_node();
-        output += str(current.get_value()) + "\n";
-        return output;
-
-    def remove_node(self, value_to_remove):
-        print('Removing node with value: {}'.format(value_to_remove));
-        
-        current = self.head_node;
-        next = current.get_next_node()
-        if current.get_value == value_to_remove:
-            self.head_node = next;
-        else:
-            while next.get_value() != value_to_remove:
-                current = next;
-                next = next.get_next_node();
-                if next == None:
-                    print('No node to remove with value: {}'.format(value_to_remove));
-                    return;
-            current.set_next_node(next.get_next_node());
+    def __iter__(self):
+        current_node = self.head_node
+        while(current_node):
+            yield current_node.get_value()
+        current_node = current_node.get_next_node()
--- Node.py
+++ Node.py
@@ -1,13 +1,13 @@
 class Node:
-    def __init__(self, value, next_node = None):
-        self.value = value;
-        self.next_node = next_node;
-
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
+        
     def get_value(self):
-        return self.value;
-
+        return self.value
+    
     def get_next_node(self):
-        return self.next_node;
-
+        return self.next_node
+    
     def set_next_node(self, next_node):
-        self.next_node = next_node;
+        self.next_node = next_node
