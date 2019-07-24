e809162a36990a536859a14bbc93be2cfbe53c71
Nick Burrin <NickBurrin18@gmail.com>
2019-07-23 20:27:55 -0700

    Initial commit with trees

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 from hashmap import HashMap
+from treeNode import TreeNode
 
-hashMap = HashMap(20)
-hashMap.assign('gabbro', 'igneous')
-hashMap.assign('sandstone', 'sedimentary')
-hashMap.assign('gneiss', 'metamorphic')
+# hashMap = HashMap(20)
+# hashMap.assign('gabbro', 'igneous')
+# hashMap.assign('sandstone', 'sedimentary')
+# hashMap.assign('gneiss', 'metamorphic')
 
-print(hashMap.retrieve('gabbro'))
-print(hashMap.retrieve('sandstone'))
-print(hashMap.retrieve('gneiss'))
+# print(hashMap.retrieve('gabbro'))
+# print(hashMap.retrieve('sandstone'))
+# print(hashMap.retrieve('gneiss'))
+
+seed = TreeNode('Nick')
---
+++ treeNode.py
@@ -0,0 +1,4 @@
+class TreeNode:
+    def __init__(self, value):
+        print("initializing node...")
+        self.value = value
